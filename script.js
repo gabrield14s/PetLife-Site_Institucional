f34aa08a5806a82a569f47c952dc782fa73be95d
gabrield14s <gabrieldiasda321@gmail.com>
2023-12-08 23:12:00 -0300

    update-JS-and-small-mudances-in-html&css

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const btnMenu = document.querySelector("#btn-hamburger-icon button");
 const menu = document.querySelector("#menu");
+const duvidas = document.querySelectorAll(".duvida");
 
 function buttonMenu(){
-
   if (menu.style.display == "flex" && btnMenu.style.zIndex == "2"){
     menu.style.display = "none"
     btnMenu.style.zIndex = "1"
@@ -10,5 +10,10 @@ function buttonMenu(){
       menu.style.display = "flex"
       btnMenu.style.zIndex = "2"
   }
-  
 };
+
+duvidas.forEach(element => {
+  element.addEventListener('click', () => {
+    element.classList.toggle('ativa');
+  })
+});
